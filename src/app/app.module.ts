06cee19586f692c05be90cdcbe9be2a99a4fb7e8
mkinoshita22 <mkinoshita@csumb.edu>
2018-11-02 14:38:34 -0700

    First push

---
+++ src/app/app.module.ts
@@ -0,0 +1,43 @@
+import { ThirdPageComponent } from './third-page/third-page.component';
+import { FirstPageComponent } from './first-page/first-page.component';
+import { RouterModule, Routes } from '@angular/router';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MyNavComponent } from './my-nav/my-nav.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule } from '@angular/material';
+import { SecondPageComponent } from './second-page/second-page.component';
+
+const appRoutes: Routes = [
+  {path: 'first-page', component: FirstPageComponent},
+  {path: 'second-page', component: SecondPageComponent},
+  {path: 'third-page', component: ThirdPageComponent}
+
+];
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MyNavComponent,
+    FirstPageComponent,
+    SecondPageComponent,
+    ThirdPageComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    RouterModule.forRoot(appRoutes),
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }

d7491af143615192c578b93d0b1226ec319528bd
Faith <25356566+faithshatt0@users.noreply.github.com>
2018-12-04 21:58:42 -0800

    Added images I think

--- store/src/app/product-list/product-list.component.ts
+++ store/src/app/product-list/product-list.component.ts
@@ -4,7 +4,8 @@ import { ProductService } from './product.service';
 
 @Component({
   selector: 'app-product-list',
-  templateUrl: './product-list.component.html'
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css']
 })
 
 export class ProductListComponent implements OnInit {

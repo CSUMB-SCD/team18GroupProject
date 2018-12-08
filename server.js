9c63a46eeb0d21982aa815c807a73816d924b343
Eros Gonzalez <ErosGonzalez@Eross-MacBook-Pro.local>
2018-12-07 19:53:52 -0800

    cors fix

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const path = require('path');
 
 const app = express();
 
+var cors = require('cors')
+
 // Serve only the static files form the dist directory
 app.use(express.static('./dist/cst438Group'));
 
@@ -11,6 +13,15 @@ app.get('/*', function(req,res) {
     
 res.sendFile(path.join('./dist/cst438Group/index.html'));
 });
+
+app.use(cors())
+
+app.get('/products/:id', function (req, res, next) {
+  res.json({msg: 'This is CORS-enabled for all origins!'})
+})
+app.listen(80, function () {
+  console.log('CORS-enabled web server listening on port 80')
+})
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);

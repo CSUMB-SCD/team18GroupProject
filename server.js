6812ee2d29220fac817a1657d48c93a5f4513741
Eros Gonzalez <ErosGonzalez@Eross-MacBook-Pro.local>
2018-12-07 17:18:49 -0800

    heroku

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('/dist/cst438Group'));
+app.use(express.static('./dist/cst438Group'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join('/dist/cst438Group/index.html'));
+res.sendFile(path.join('./dist/cst438Group/index.html'));
 });
 
 // Start the app by listening on the default Heroku port

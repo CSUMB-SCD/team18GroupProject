0d12960a92e8b45b2407f211d141cb031eaf0cec
Eros Gonzalez <ErosGonzalez@Eross-MacBook-Pro.local>
2018-12-07 20:36:23 -0800

    cros

--- db/src/main/java/edu/csumb/cst438/projectdb/ProjectController.java
+++ db/src/main/java/edu/csumb/cst438/projectdb/ProjectController.java
@@ -18,42 +18,36 @@ public class ProjectController {
     @Autowired
     IUserRepository userRepository;
 
-    @CrossOrigin(origin = "*")
     @GetMapping ("/allProducts")
     public List<Product> getAll () {
         List<Product> result = productRepository.findAll();
         return result;
     }
-
-    @CrossOrigin(origin = "*")	
+    	
     @GetMapping("/id/{id}")
     public Product getProductById (@PathVariable String id) {
         Product result = productRepository.findByRepoId(id);
         return result;
     }
 
-    @CrossOrigin(origin = "*")
     @GetMapping("/users")
     public List<User> getAllUsers () {
         List<User> result = userRepository.findAll();
         return result;
     }
 
-    @CrossOrigin(origin = "*")
     @PostMapping("/users")
     public User createUser(String username, String password) {
         User user = new User(username, password);
         return userRepository.save(user);
     }
 
-    @CrossOrigin(origin = "*")
     @GetMapping("/username/{username}")
     public User getUsername(String username) {
         User result = userRepository.findByUsername(username);
         return result;
     }
-
-    @CrossOrigin(origin = "*")	
+    	
     @GetMapping("/users/{id}")
     public User getUserById (@PathVariable String id) {
         User result = userRepository.findByRepoId(id);

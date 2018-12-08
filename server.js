e0fb4c9e6e60f2cc154dbdf44483eefd79a2884b
Eros Gonzalez <ErosGonzalez@Eross-MacBook-Pro.local>
2018-12-07 16:26:15 -0800

    heroku

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 //Install express server
 const express = require('express');
-const path = require('path');
+//const path = require('path');
 
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/cst438-group'));
+app.use(express.static('/dist'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/cst438-group/index.html'));
+res.sendFile(path.join('/dist/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
